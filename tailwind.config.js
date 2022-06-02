7a49cfd9f6f2c18dca2b81418209db704a9a8b2b
Louis Cutner <lcutner@me.com>
2022-06-02 09:43:17 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      height: {
+        "10p": "20%",
+        500: "300rem",
+        85: "21rem",
+      },
+      width: {
+        "1280px": "1280px",
+      },
+      margin: {
+        88: "22rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
